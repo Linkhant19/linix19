7edd1fdd7c31cfa517a15296ca3aa6a9a6196c9a
Lin Khant Ko <lkk19@bu.edu>
2023-11-30 15:45:59 -0500

    added one square with link and some design changes to first.html

--- docs/first.js
+++ docs/first.js
@@ -1,3 +1,11 @@
 function redirectTo(url) {
     window.location.href = url;
 }
+
+function redirectTo(url) {
+    if(url.startsWith('http://') || url.startsWith('https://')) {
+        window.open(url, '_blank'); 
+    } else {
+        window.location.href = url; 
+    }
+}
